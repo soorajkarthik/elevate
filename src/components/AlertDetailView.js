c0d49188ca3fb0978eacde5c20a4c05136025914
Sooraj Karthik <soorajkarthik15@gmail.com>
2021-04-11 16:45:14 -0400

    made some basic changes. need to do more backend work before I start doing this UI stuff

--- src/components/AlertDetailView.js
+++ src/components/AlertDetailView.js
@@ -6,7 +6,17 @@ class AlertDetailView extends Component {
     static contextType = AppContext;
 
     render() {
-        return <View />;
+        let focusedAlert = this.context.getState().focusedAlert;
+
+        return focusedAlert ? (
+            <View style={ { ...this.props.style, ...styles.container } }>
+                <Text></Text>
+            </View>
+        ) : (
+            <View style={ { ...this.props.style, ...styles.container } }>
+                <Text>Click on an alert marker on the map to view more details!</Text>
+            </View>
+        );
     }
 }
 
